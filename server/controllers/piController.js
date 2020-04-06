652de0c2546005acdbb6b2b5921a4b5c2a7fe30a
Sam Selfridge <Sam.Selfridge@gmail.com>
2020-04-05 21:22:44 -0700

    new init

--- server/controllers/piController.js
+++ server/controllers/piController.js
@@ -13,7 +13,7 @@ const piController = {
     red: null,
     yellow: null,
     green: null,
-    motion: MOVEMENT,
+    motion: 3,
     doorTime: 0, // time door has been in current state
     doorState: OPEN, // current door state
 
