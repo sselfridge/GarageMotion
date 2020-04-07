dc702734b3b7c41f6397698fe03bc0f453bf05cc
Sam Selfridge <Sam.Selfridge@gmail.com>
2020-04-06 20:33:39 -0700

    removed

--- server/bin/mongoose.js
+++ server/bin/mongoose.js
@@ -2,10 +2,17 @@ const mongoose = require("mongoose");
 const config = require("../../config/keys");
 
 const startMongoose = () => {
-  mongoose.connect(config.dbKeys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
-  mongoose.connection.once("open", (error, client) => {
-    console.log("DB Connected");
+  const uri = config.dbKeys.mongoURI
+  mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  console.log("Starting Connection");
+  var db = mongoose.connection;
+  db.on("error", console.error.bind(console, "connection error:"));
+  db.once("open", function() {
+    console.log("DB connected");
   });
 };
 
 module.exports = startMongoose;
+
+
+
